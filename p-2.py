c24d69a2dda9508b21a29fd54613bd8c7d64f6db
Harshalmakwana01 <makwanaharshal1510@gmail.com>
2025-12-31 08:24:13 +0530

    Add files via upload

---
+++ p-2.py
@@ -0,0 +1,78 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#assigment operator
+x=10
+print(x)
+10
+#unary minus
+a=5
+print(-a)
+-5
+#relational
+p=10
+q=5
+print(p<q)
+False
+print(p==q)
+False
+#logical
+x=true
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    x=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+x=True
+y=False
+print(x and y)
+False
+print
+<built-in function print>
+(
+print(x or y)
+True
+print(not x)
+False
+#boolean
+is pass=true
+SyntaxError: invalid syntax
+is pass=True
+SyntaxError: invalid syntax
+is_pass=true
+Traceback (most recent call last):
+  File "<pyshell#22>", line 1, in <module>
+    is_pass=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+is_pass=True
+print(is pass) #o/p=True
+SyntaxError: invalid syntax
+print(is_pass)
+True
+#bitwise
+a=5
+b=3
+print(a&b)
+1
+print(p)
+10
+print(a|b)
+7
+#membership
+number=[1,2,3,4,5,6]
+print(2 in number)
+True
+print(5 not in number)
+False
+#identity
+x=10
+y=10
+print(x is y)
+True
+print(x is not y)
+False
+#arithmetic
+a=5
+b=5
+print(x+y)
+20
+print(x-y)
+0
