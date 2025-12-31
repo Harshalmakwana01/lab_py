e43f5dc21c8816f70bc000eb90c4bb0cfd77e5fc
Harshalmakwana01 <makwanaharshal1510@gmail.com>
2025-12-31 08:23:41 +0530

    Add files via upload

---
+++ p-1.py
@@ -0,0 +1,63 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a=12
+b=12
+x=a+b
+print(x)
+24
+
+x=a-b
+print(x)
+0
+
+x=a*b
+print(x)
+144
+
+x=a/b
+print(x)
+1.0
+
+x="atmiya university"
+print(x)
+atmiya university
+
+x=["appple","banana","cherry"]
+print(x)
+['appple', 'banana', 'cherry']
+
+x="true"
+print(x)
+true
+
+x=1j
+print(x)
+1j
+
+a=10
+b=5
+print(a>b and b>0)
+True
+
+print(a>b and b>6)
+False
+
+print(a>b ^ b>2)
+False
+
+a=[1,2]
+b=a
+c=[1,2]
+print(a is b)
+True
+print(a is c)
+False
+
+a=5
+a+=5
+print(a)
+10
+
+a-=5
+print(a)
+5
