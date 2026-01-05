e82ea59dbab4ab4979415ab13b9f24f64b99a508
Harshalmakwana01 <makwanaharshal1510@gmail.com>
2026-01-05 11:20:33 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,37 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x="hello world"
+print(x)
+hello world
+x=20
+print(x)
+20
+x=20.85
+print(x)
+20.85
+X=True
+print(x)
+20.85
+x=True
+print(x)
+True
+x=("apple","banana","cherry")
+print(x)
+('apple', 'banana', 'cherry')
+x=['apple','banana','cherry']
+print(x)
+['apple', 'banana', 'cherry']
+x={"name":"john","age";36}
+SyntaxError: ':' expected after dictionary key
+x={"name":"john","age":36}
+print(x)
+{'name': 'john', 'age': 36}
+a=?
+SyntaxError: invalid syntax
+a=
+SyntaxError: invalid syntax
+a=?
+SyntaxError: invalid syntax
+x=1j
+print(x)
+1j
