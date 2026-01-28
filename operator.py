36098116823a1b9381d18fda70046d9eb113de76
Harshalmakwana01 <makwanaharshal1510@gmail.com>
2026-01-28 09:03:54 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,25 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a=7
+b=4
+print(a&b)
+4
+a=3
+b=5
+print(a&b)
+1
+a=5
+b=2
+print(a^b)
+7
+a=6
+b=3
+print(a^b)
+5
+a=4
+b=7
+print(a|b)
+7
+a=5
+print(~a)
+-6
